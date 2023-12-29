0ad11b4daed494c633835e48f645062fe41873d5
MINSEON\minseon <duiph558291@gmail.com>
2023-12-29 15:36:32 +0900

    commit message

---
+++ src/main/java/com/codong/spotifymate/SpotifymateApplication.java
@@ -0,0 +1,13 @@
+package com.codong.spotifymate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifymateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotifymateApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codong/spotifymate/SpotifymateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codong.spotifymate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotifymateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
